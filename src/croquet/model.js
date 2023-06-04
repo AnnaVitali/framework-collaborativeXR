6f1ad4f03ed1ea1567f1d87a1ec03431b44d3ad1
AnnaVitali <anna.vitali4@studio.unibo.it>
2023-06-04 16:45:32 +0200

    feature: add croquet element model and view

---
+++ src/croquet/model.js
@@ -0,0 +1,106 @@
+import {EventEmitter} from "../event/event_emitter.js";
+
+const canvas = document.getElementById("renderCanvas");
+
+class RootModel extends Croquet.Model {
+
+    /**
+     * Initialize the Model.
+     * */
+    init() {
+        this.linkedViews = [];
+        this.eventEmitter = new EventEmitter();
+
+        this.subscribe(this.sessionId, "view-join", this.viewJoin);
+        this.subscribe(this.sessionId, "view-exit", this.viewDrop);
+
+        this.#initializeScene();
+        this.#activateRenderLoop();
+
+    }
+
+    /**
+     * Handle a new connected view.
+     * @param {any} viewId the id of the new view connected.
+     */
+    viewJoin(viewId){
+        console.log("MODEL: received view join");
+        this.linkedViews.push(viewId);
+        console.log("Is user manipulating " + this.isUserManipulating);
+    }
+
+    /**
+     * Handle the view left event.
+     * @param {any} viewId the id of the outgoing view.
+     */
+    viewDrop(viewId){
+        console.log("MODEL: received view left");
+        this.linkedViews.splice(this.linkedViews.indexOf(viewId),1);
+        if(this.linkedViews.length === 0){
+            this.destroy();
+        }
+    }
+
+    #initializeScene(){
+        this.engine = new BABYLON.Engine(canvas, true);
+        this.scene = new BABYLON.Scene(this.engine);
+        this.scene.clearColor = new BABYLON.Color3.Black;
+
+        const alpha =  3 * Math.PI/2;
+        const beta = Math.PI/50;
+        const radius = 220;
+        const target = new BABYLON.Vector3(0, 0, 0);
+
+        const camera = new BABYLON.ArcRotateCamera("Camera", alpha, beta, radius, target, this.scene);
+        camera.attachControl(canvas, true);
+
+        const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0));
+        light.intensity = 1;
+
+        this.GUIManager = new BABYLON.GUI.GUI3DManager(this.scene);
+        this.GUIManager.useRealisticScaling = true;
+    }
+
+    async #createWebXRExperience() {
+        const supported = await BABYLON.WebXRSessionManager.IsSessionSupportedAsync('immersive-ar')
+        let xrHelper
+
+        if (supported) {
+            console.log("IMMERSIVE AR SUPPORTED");
+            xrHelper = await this.scene.createDefaultXRExperienceAsync({
+                uiOptions: {
+                    sessionMode: 'immersive-ar',
+                    referenceSpaceType: "local-floor"
+                }
+            });
+        } else {
+            console.log("IMMERSIVE VR SUPPORTED")
+            xrHelper = await this.scene.createDefaultXRExperienceAsync({
+                uiOptions: {
+                    sessionMode: 'immersive-vr',
+                }
+            });
+        }
+
+        try {
+            xrHelper.baseExperience.featuresManager.enableFeature(BABYLON.WebXRFeatureName.HAND_TRACKING, "latest", { xrInput: xr.input });
+        } catch (err) {
+            console.log("Articulated hand tracking not supported in this browser.");
+        }
+
+        return this.scene;
+    }
+
+    #activateRenderLoop() {
+        this.#createWebXRExperience().then(sceneToRender => {
+            this.engine.runRenderLoop(() => sceneToRender.render());
+        });
+    }
+
+}
+
+
+RootModel.register("RootModel");
+
+
+export { RootModel };
