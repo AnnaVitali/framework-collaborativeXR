d59513fb7ee407509a1f79e719559ab0c599a7d2
AnnaVitali <anna.vitali4@studio.unibo.it>
2023-06-07 16:27:33 +0200

    test: update experiments

--- src/session/sesssion_manager.js
+++ src/session/sesssion_manager.js
@@ -2,6 +2,7 @@ import {CroquetSession} from "../croquet/croquet_session.js";
 class SessionManager {
     constructor(){
         this.debug = true;
+        this.sessionStarted = false;
     }
 
     async startSession(apiKey, appId){
@@ -9,7 +10,6 @@ class SessionManager {
             throw new Error("parameters apiKey and appId can't be undefined!");
         }
         if(this.debug) {
-            console.log("SessionManager: session started");
             console.log("apiKey: " + apiKey);
             console.log("appId: " + appId);
         }
@@ -17,7 +17,18 @@ class SessionManager {
         const croquetSession = new CroquetSession();
         await croquetSession.sessionJoin(apiKey, appId);
 
-        return new Promise((resolve) => resolve(true));
+        return new Promise((resolve) =>
+        {
+            if(this.debug) {
+                console.log("SessionManager: session started true");
+            }
+            this.sessionStarted = true;
+            resolve(true);
+        });
+    }
+
+    isSessionStarted(){
+        return this.sessionStarted;
     }
 }
 
