47649eb9335606665701a4c2f56efdff4522e217
AnnaVitali <anna.vitali4@studio.unibo.it>
2023-07-18 10:17:59 +0200

    feat: change mechanism to update the variable

--- src/core/synchronizedVariable/synchronized_variable.js
+++ src/core/synchronizedVariable/synchronized_variable.js
@@ -1,4 +1,3 @@
-import {elementChecker} from "../utility/element_checker.js";
 import {coreEventManager} from "../utility/core_event_manager.js";
 import {synchronizedElementUpdater} from "../utility/synchronized_element_updater.js";
 
@@ -12,15 +11,8 @@ class SynchronizedVariable{
      * @param value the value to assign.
      */
     constructor(name, value) {
-        if(elementChecker.verifyNameAlreadyExist(name)){
-            throw new Error("This name was already used!")
-        }
-
         this._name = name;
         this._value = value;
-
-        synchronizedElementUpdater.addSynchronizedVariable(this);
-        coreEventManager.sendEvent("createSynchronizedVariable", JSON.stringify(this));
     }
 
     /**
@@ -28,13 +20,12 @@ class SynchronizedVariable{
      * @param value
      */
     set value(value) {
-        if(synchronizedElementUpdater.update) {
-            this._value = value;
-            coreEventManager.sendEvent("valueChange", JSON.stringify({
-                variableName: this.name,
-                value: this.value
-            }));
-        }
+        this._value = value;
+        coreEventManager.sendEvent("valueChange", JSON.stringify({
+            variableName: this.name,
+            value: this.value
+        }));
+
     }
 
     /**
