b634dc1e4e7a8360c478582376081956e2ab79a9
AnnaVitali <anna.vitali4@studio.unibo.it>
2023-07-23 19:45:10 +0200

    refactor: refactore code example

--- app.js
+++ app.js
@@ -14,11 +14,27 @@ https.createServer(
     , app)
     .listen(port, host, () => {
         console.log('Server started at https://' + host + ':' + port);
+        console.log('Visit:')
+        console.log('https://' + host + ':' + port + "/exampleAnimation");
+        console.log('https://' + host + ':' + port + "/exampleRobot");
+        console.log('https://' + host + ':' + port + "/exampleAnimal");
     });
 
 app.use("/", express.static(path.join(__dirname)));
 console.log("static path: " + path.join(__dirname));
 
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, '/html/template.html'));
+    res.sendFile(path.join(__dirname, '/src/html/template.html'));
+})
+
+app.get("/exampleAnimation", function (req, res) {
+    res.sendFile(path.join(__dirname, '/src/html/example_animation.html'));
+})
+
+app.get("/exampleRobot", function (req, res) {
+    res.sendFile(path.join(__dirname, '/src/html/example_robot.html'));
+})
+
+app.get("/exampleAnimal", function (req, res) {
+    res.sendFile(path.join(__dirname, '/src/html/example_animal.html'));
 })
