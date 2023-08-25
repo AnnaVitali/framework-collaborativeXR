129275347827690c387eaa699d2446f9a7b8b18b
AnnaVitali <anna.vitali4@studio.unibo.it>
2023-08-25 18:13:28 +0200

    fix: update compiled script

--- compiled/collaborativeXR_compiled.js
+++ compiled/collaborativeXR_compiled.js
@@ -1,4 +1,62 @@
 /**
+ * Class that represents an event bus, by which send and receive event specifying an handler.
+ */
+class EventBus {
+  /**
+   * Empty constructor of the class.
+   */
+  constructor() {
+    this.listeners = [];
+  }
+
+  /**
+   * Emit a new event on the event bus.
+   * @param eventName {String} the name of the event.
+   * @param data {String} the data to send.
+   */
+  emit(eventName, data) {
+    this.listeners.filter(({
+                             name
+                           }) => name === eventName).forEach(({
+                                                                callback
+                                                              }) => {
+      setTimeout(callback.apply(this, [data]), 0);
+    });
+  }
+
+  /**
+   * Handle an event received
+   * @param name {String} the name of the event.
+   * @param callback the callback to call when the event arrives.
+   */
+  on(name, callback) {
+    if (typeof callback === 'function' && typeof name === 'string') {
+      this.listeners.push({
+        name,
+        callback
+      });
+    }
+  }
+
+  /**
+   * Unsubscribe the relative callback to the event.
+   * @param eventName {String} name of the event.
+   * @param callback the callback to remove.
+   */
+  off(eventName, callback) {
+    this.listeners = this.listeners.filter(listener => !(listener.name === eventName && listener.callback === callback));
+  }
+
+  /**
+   * Destroy the event bus.
+   */
+  destroy() {
+    this.listeners.length = 0;
+  }
+}
+const eventBus = new EventBus();
+export { eventBus };
+/**
  * Class representing an animation that can be associated with an element.
  */
 class Animation {
@@ -834,64 +892,6 @@ class SynchronizedElementUpdater {
 }
 const synchronizedElementUpdater = new SynchronizedElementUpdater();
 export { synchronizedElementUpdater };
-/**
- * Class that represents an event bus, by which send and receive event specifying an handler.
- */
-class EventBus {
-  /**
-   * Empty constructor of the class.
-   */
-  constructor() {
-    this.listeners = [];
-  }
-
-  /**
-   * Emit a new event on the event bus.
-   * @param eventName {String} the name of the event.
-   * @param data {String} the data to send.
-   */
-  emit(eventName, data) {
-    this.listeners.filter(({
-      name
-    }) => name === eventName).forEach(({
-      callback
-    }) => {
-      setTimeout(callback.apply(this, [data]), 0);
-    });
-  }
-
-  /**
-   * Handle an event received
-   * @param name {String} the name of the event.
-   * @param callback the callback to call when the event arrives.
-   */
-  on(name, callback) {
-    if (typeof callback === 'function' && typeof name === 'string') {
-      this.listeners.push({
-        name,
-        callback
-      });
-    }
-  }
-
-  /**
-   * Unsubscribe the relative callback to the event.
-   * @param eventName {String} name of the event.
-   * @param callback the callback to remove.
-   */
-  off(eventName, callback) {
-    this.listeners = this.listeners.filter(listener => !(listener.name === eventName && listener.callback === callback));
-  }
-
-  /**
-   * Destroy the event bus.
-   */
-  destroy() {
-    this.listeners.length = 0;
-  }
-}
-const eventBus = new EventBus();
-export { eventBus };
 
 /**
  * Class responsible to manage the Croquet session.
